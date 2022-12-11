27ea45990fdb362b858722252f8a515a2006bb6b
Stanislava Pavlova <stanislavap2002@gmail.com>
2022-12-11 19:20:38 +0200

    task

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,47 +1,67 @@
-import EventEmitter from "eventemitter3";
-import image from "../images/planet.svg";
+import EventEmitter from 'eventemitter3';
+import image from '../images/planet.svg';
 
 export default class Application extends EventEmitter {
   static get events() {
     return {
-      READY: "ready",
+      READY: 'ready',
     };
   }
 
   constructor() {
     super();
 
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.innerHTML = this._render({
-      name: "Placeholder",
-      terrain: "placeholder",
-      population: 0,
-    });
-
-    document.body.querySelector(".main").appendChild(box);
+    const _loading = document.body.querySelector('progress');
 
     this.emit(Application.events.READY);
   }
 
+  async _load() {
+    const apiUrl = await fetch('https://swapi.boom.dev/api/planets/');
+    const data = await apiUrl.json();
+    return data;
+  }
+
+  _create() {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.innerHTML = this._render({
+      name: 'Placeholder',
+      terrain: 'placeholder',
+      population: 0,
+    });
+
+    document.body.querySelector('.main').appendChild(box);
+  }
+
   _render({ name, terrain, population }) {
     return `
-<article class="media">
-  <div class="media-left">
-    <figure class="image is-64x64">
-      <img src="${image}" alt="planet">
-    </figure>
-  </div>
-  <div class="media-content">
-    <div class="content">
-    <h4>${name}</h4>
-      <p>
-        <span class="tag">${terrain}</span> <span class="tag">${population}</span>
-        <br>
-      </p>
-    </div>
-  </div>
-</article>
+      <article class="media">
+        <div class="media-left">
+          <figure class="image is-64x64">
+            <img src="${image}" alt="planet" />
+          </figure>
+        </div>
+        <div class="media-content">
+          <div class="content">
+            <h4>${name}</h4>
+            <p>
+              <span class="tag">${terrain}</span>
+              <span class="tag">${population}</span>
+              <br />
+            </p>
+          </div>
+        </div>
+      </article>
     `;
+  }
+
+  async _startLoading() {
+    await this._load();
+    this._loading.style.visibility = 'visible';
+  }
+
+  _stopLoading() {
+    _loading.style.visibility = 'hidden';
   }
 }
